5781550bc32c9bb03bd59bd8bbf41e6c7b30c9a1
mlleo <0x01.leo@gmail.com>
2021-03-08 08:56:31 +0900

    board paging and search

--- demo/src/main/java/com/example/demo/controller/BoardController.java
+++ demo/src/main/java/com/example/demo/controller/BoardController.java
@@ -16,10 +16,13 @@ public class BoardController {
 
     /* 게시글 목록 */
     @GetMapping("/")
-    public String list(Model model) {
-        List<BoardDto> boardList = boardService.getBoardlist();
+    public String list(Model model, @RequestParam(value="page", defaultValue = "1") Integer pageNum) {
+        List<BoardDto> boardList = boardService.getBoardlist(pageNum);
+        Integer[] pageList = boardService.getPageList(pageNum);
 
         model.addAttribute("boardList", boardList);
+        model.addAttribute("pageList", pageList);
+
         return "board/list.html";
     }
 
@@ -64,4 +67,15 @@ public class BoardController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/board/search")
+    public String search(@RequestParam(value="keyword") String keyword, Model model) {
+        List<BoardDto> boardDtoList = boardService.searchPosts(keyword);
+
+        model.addAttribute("boardList", boardDtoList);
+
+        return "board/list.html";
+    }
+
+
 }
