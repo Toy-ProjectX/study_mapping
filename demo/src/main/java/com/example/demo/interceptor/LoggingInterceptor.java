da1f30faf28e074c9b94fcc98da59dc65249c619
mario3316 <wlshgktm@gmail.com>
2021-03-13 19:05:13 +0900

    Debug : csrf token submission 20210313

---
+++ demo/src/main/java/com/example/demo/interceptor/LoggingInterceptor.java
@@ -0,0 +1,33 @@
+package com.example.demo.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("PREHANDLE : {}", request.getRequestURI());
+//        return super.preHandle(request, response, handler);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.info("POSTHANDLE Response Status : {}", response.getStatus());
+//        super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.info("AFTER COMPLETION");
+    }
+}
